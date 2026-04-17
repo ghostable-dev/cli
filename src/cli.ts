3fc847902631f9699956f79950409e85e6275dd2
Joe Rucci <rucci.joe@gmail.com>
2026-04-17 12:09:29 -0400

    chore(cli): bump version to v2.6.1

--- src/cli.ts
+++ src/cli.ts
@@ -119,7 +119,7 @@ async function maybePromptInteractiveSubcommand(
 
 const program = new Command();
 program.name('ghostable').description('Manage Ghostable environment secrets from the CLI');
-program.version('v2.6.0');
+program.version('v2.6.1');
 await registerAllCommands(program);
 program.configureHelp({
 	subcommandTerm: (cmd) => {
